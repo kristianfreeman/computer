7a762e1aa7b00048f4732e9b51184063968c9a26
Kristian Freeman <kristian@kristianfreeman.com>
2024-11-11 14:39:38 -0600

    2024-11-11-14:39:38

--- sauron/configuration.nix
+++ sauron/configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "smaug"; # Define your hostname.
+  networking.hostName = "sauron"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
