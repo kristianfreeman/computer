5c835f5847b637a6ee71e48a779b289a0c35fce2
Kristian Freeman <kristian@kristianfreeman.com>
2024-11-20 19:37:41 -0600

    add avahi

--- hosts/sauron/configuration.nix
+++ hosts/sauron/configuration.nix
@@ -77,6 +77,16 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kristian = {
     isNormalUser = true;
