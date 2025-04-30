d505734412bb17d5898f03c14888624a4a1a9951
Kristian Freeman <kristian@kristianfreeman.com>
2025-04-30 13:10:37 -0500

    2025-04-30-13:10:37

--- hosts/cf/configuration.nix
+++ hosts/cf/configuration.nix
@@ -3,23 +3,11 @@
 let 
 in
 {
-  # Import common macOS configuration
   imports = [
     ../darwin-common.nix
     ../darwin-homebrew.nix
     ../roles/development.nix
   ];
 
-  # Host-specific overrides and additions
-  
-  # Host-specific Homebrew casks
-  homebrew.casks = [
-    "boltai"
-    "flux"
-    "kitty"
-    "macwhisper"
-    "mochi"
-    "plexamp"
-    "screen-studio"
-  ];
+  homebrew.casks = [];
 }
